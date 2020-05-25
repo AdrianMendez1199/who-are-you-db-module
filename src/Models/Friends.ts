c981c6a6212f67f810cd36e453e9a5088c918b0e
Adrian Mendez <mendezadrian149@gmail.com>
2020-05-25 19:50:48 -0400

    adding schema friends

---
+++ src/Models/Friends.ts
@@ -0,0 +1,23 @@
+/**
+ * return model message
+ * @param mongoose
+ */
+export default function message(mongoose: any): object {
+  const schema = mongoose.Schema;
+
+  const friendSchema = new schema({
+    requester: { type: schema.Types.ObjectId, ref: 'Users' },
+    recipient: { type: schema.Types.ObjectId, ref: 'Users' },
+    status: {
+      type: Number,
+      enums: [
+        0,    // 'add friend',
+        1,    // 'requested',
+        2,    // 'pending',
+        3,    // 'friends'
+      ],
+    },
+  },                              { timestamps: true });
+
+  return mongoose.model('message', friendSchema);
+}
--- src/Models/User.ts
+++ src/Models/User.ts
@@ -6,7 +6,13 @@ export default function user(mongoose: any): object {
     lastname: { type: String, required: true },
     password: { type: String, required: true },
     username: { type: String, required: true },
-  });
+    isActive: { type: Number,
+      enums: [
+        0,    // 'Active',
+        1,    // 'Inactive',
+      ] },
+    friends: [{ type: schema.Types.ObjectId, ref: 'User' }],
+  },                            { timestamps: true });
 
   return mongoose.model('User', userSchema);
 }
