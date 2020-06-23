3b4a216feebcce052c1f8705655fef351f807edb
Adrian Mendez <mendezadrian149@gmail.com>
2020-06-23 18:16:26 -0400

    using promisify to return promise

--- src/services/user/user-client.ts
+++ src/services/user/user-client.ts
@@ -1,8 +1,7 @@
 import path from 'path';
 import { loadPackageDefinition, credentials } from 'grpc';
 import { loadSync } from '@grpc/proto-loader';
-
-import { User, Login } from './types';
+import { promisify } from 'util';
 
 const proto = loadSync(path.resolve(__dirname, '../../proto/user.proto'), {
   keepCase: true,
@@ -14,43 +13,8 @@ const usersServiceClient: any = loadPackageDefinition(proto).user;
 const userService = usersServiceClient.UsersService;
 const userClient = new userService('0.0.0.0:50051', credentials.createInsecure());
 
-const createUser = (data: User) => new Promise((resolve, reject) => {
-  // tslint:disable-next-line: ter-prefer-arrow-callback
-  userClient.createUser({ ...data }, function (err: any, response: any) {
-    // tslint:disable-next-line: whitespace
-    if (err) {
-      return reject(err);
-    }
-    return resolve(response);
-  });
-});
-
-const getUserByUsername = (data: { username: string }) => new Promise((resolve, reject) => {
-  const { username } = data;
-  // tslint:disable-next-line: ter-prefer-arrow-callback
-  userClient.getUserByUsername({ username }, function (err: any, response: any) {
-    // tslint:disable-next-line: whitespace
-    if (err) {
-      return reject(err);
-    }
-    return resolve(response.user);
-  });
-});
-
-const login = (data: Login) => new Promise((resolve, reject) => {
-  const { username, password } = data;
-  // tslint:disable-next-line: ter-prefer-arrow-callback
-  userClient.login({ username, password }, function (err: any, response: any) {
-    if (err) {
-      return reject(err);
-    }
-    return resolve(response.auth);
-  });
-});
-
-
 export default {
-  createUser,
-  getUserByUsername,
-  login,
+  createUser: promisify(userClient.getUserByUsername).bind(userClient),
+  getUserByUsername: promisify(userClient.getUserByUsername).bind(userClient),
+  login: promisify(userClient.getUserByUsername).bind(userClient),
 };
